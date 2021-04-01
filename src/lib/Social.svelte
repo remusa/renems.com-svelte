55bc3a8800256c47a51b3378edafe57c7ac3aa2a
R Sánchez <9169831+remusa@users.noreply.github.com>
2021-04-01 01:17:24 -0600

    feat: Add social icons

---
+++ src/lib/Social.svelte
@@ -0,0 +1,9 @@
+<script>
+	import SocialIcon from './SocialIcon.svelte'
+</script>
+
+<div class="flex justify-between">
+	<SocialIcon alt="mail icon" src="static/icons/mail.svg" to="/mail" name="Mail" />
+	<SocialIcon alt="github icon" src="static/icons/github.svg" to="/github" name="GitHub" />
+	<SocialIcon alt="linkedin icon" src="static/icons/linkedin.svg" to="/linkedin" name="Linkedin" />
+</div>
---
+++ src/lib/SocialIcon.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Icon from './Icon.svelte'
+
+	export let src: string
+	export let alt: string
+	export let to: string
+	export let name: string
+</script>
+
+<a
+	class="border-bottom group px-2 flex flex-col items-center hover:translate-y-4 transition duration-500"
+	target="_blank"
+	rel="noopener noreferrer"
+	href={to}
+>
+	<Icon {alt} {src} />
+	<p class="text-xs font-semibold pb-1 text-primary transition duration-500 opacity-0 group-hover:opacity-100">
+		{name}
+	</p>
+</a>
+
+<style style lang="postcss">
+</style>
