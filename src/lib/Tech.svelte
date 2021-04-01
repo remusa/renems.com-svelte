97339cea91aebd96c1207b9cf9f3576755976a0f
R Sánchez <9169831+remusa@users.noreply.github.com>
2021-04-01 12:03:42 -0600

    refactor: Tech list

--- src/lib/Tech.svelte
+++ src/lib/Tech.svelte
@@ -1,81 +1,20 @@
 <script lang="ts">
-	const techList = [
-		{
-			name: 'JavaScript',
-			img: 'javascript.webp',
-		},
-		{
-			name: 'TypeScript',
-			img: 'typescript.webp',
-		},
-		{
-			name: 'React',
-			img: 'react.webp',
-		},
-		{
-			name: 'Node',
-			img: 'node.webp',
-		},
-		{
-			name: 'GraphQL',
-			img: 'graphql.png',
-		},
-		{
-			name: 'Python',
-			img: 'python.png',
-		},
-		{
-			name: 'Django',
-			img: 'django2.png',
-		},
-		{
-			name: 'Docker',
-			img: 'docker.webp',
-		},
-		{
-			name: 'PostgreSQL',
-			img: 'postgresql.png',
-		},
-		{
-			name: 'Apollo',
-			img: 'apollo.png',
-		},
-		{
-			name: 'Gatsby',
-			img: 'gatsby.png',
-		},
-		{
-			name: 'Next.js',
-			img: 'next.png',
-		},
-	]
+	import { techList } from 'src/data'
 </script>
 
-<div class="tech-wrapper">
+<div class="">
 	<h2 class="text-center">Favourite Technologies</h2>
 
-	<div class="tech-list pt-4">
+	<div class="tech-list my-0 mx-auto pt-4 grid gap-1">
 		{#each techList as tech}
-			<img class="logo" title={tech.name} alt={tech.name} src={''} />
+			<img class="logo object-contain w-full h-full overflow-hidden" title={tech.name} alt={tech.name} src={''} />
 		{/each}
 	</div>
 </div>
 
-<style>
-	.tech-wrapper {
-		.tech-list {
-			margin: 0 auto;
-			display: grid;
-			grid-template-columns: repeat(auto-fill, 50px);
-			gap: 2px;
-			width: calc((50px * 6) + (2px * 6));
-
-			.logo {
-				width: 100%;
-				height: 100%;
-				object-fit: contain;
-				overflow: hidden;
-			}
-		}
+<style style lang="postcss">
+	.tech-list {
+		grid-template-columns: repeat(auto-fill, 50px);
+		width: calc((50px * 6) + (2px * 6));
 	}
 </style>
