d9140bb9bafe8733e9d4b7597bc549eec2a7fed7
R Sánchez <9169831+remusa@users.noreply.github.com>
2021-04-01 01:23:54 -0600

    feat: Add Navbar component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<nav href="#header" class="w-full flex md:flex-row justify-between ">
+	<a class="text-3xl" aria-current="page" href="/">_R</a>
+
+	<div class="right ">
+		<a class="icon" href="/blog">Blog</a>
+		<a class="icon" href="/now">Now</a>
+		<a class="icon" href="/rss.xml">
+			<img class="w-5 text-white" alt="rss icon" src="static/icons/rss.svg" />
+		</a>
+	</div>
+</nav>
+
+<style style lang="postcss">
+	.right {
+		@apply flex;
+
+		& > a.icon {
+			@apply px-2 py-1 mx-1 border border-transparent bg-transparent rounded transition duration-500 hover:bg-primary;
+		}
+	}
+</style>
