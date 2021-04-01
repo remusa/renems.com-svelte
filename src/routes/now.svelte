9a6348c4fbcb678f0db7945891ef5cdf73b66c35
R Sánchez <9169831+remusa@users.noreply.github.com>
2021-04-01 01:25:04 -0600

    feat: Add Now page

---
+++ src/routes/now.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<main class="max-w-xl text-left">
+	<h1 class="text-center">Now</h1>
+
+	<p>I see being a developer as my craft, so I'm always trying to improve.</p>
+
+	<p>
+		Nowadays I'm more focused on fullstack JavaScript (mainly React, Node and GraphQL) and some Python (Django) here and
+		there.
+	</p>
+
+	<p>Currently focused on learning:</p>
+	<ul class="list-disc ">
+		<li>Elixir</li>
+		<li>Phoenix</li>
+		<li>LiveView</li>
+		<li>Absinthe</li>
+	</ul>
+</main>
+
+<style style lang="postcss">
+</style>
